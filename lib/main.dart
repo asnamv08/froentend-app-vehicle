7645f2a3560e31fffe4538a4416a9c5650860b4c
asnamv08 <asnamvarghese001@gmail.com>
2024-02-09 09:28:04 +0530

    new app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(homepage());
+}
+class homepage extends StatelessWidget {
+  const homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: menu(),
+    );
+  }
+}
